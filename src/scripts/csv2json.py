de53baef25f9d792bd6b1a30d1d40f70e2c48917
Cole Marshall <cm@colemarshall.net>
2024-01-28 13:57:18 -0600

    XML conversion, add comments to YAML

--- src/scripts/csv2json.py
+++ src/scripts/csv2json.py
@@ -2,9 +2,9 @@ import pandas as pd
 import json
 
 def csv2json(csv_file_path, json_file_path, key_column):
-    df = pd.read_csv(csv_file_path, keep_default_na=False, na_values=[''])  # Treat only empty strings as NaN
+    df = pd.read_csv(csv_file_path, keep_default_na=False, na_values=['']) # Treat only empty strings as NaN
     df = df.where(pd.notnull(df), None) # Convert NaNs to None (null)
-    data = {row[key_column]: row.to_dict() for index, row in df.iterrows()}
+    data = {row[key_column]: row.to_dict() for index, row in df.iterrows()} # Use key_column as key for each row
     with open(json_file_path, 'w') as file:
         json.dump(data, file, indent=2)
 
--- src/scripts/csv2yaml.py
+++ src/scripts/csv2yaml.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import yaml
+import argparse
 
-def csv2yaml(csv_file_path, yaml_file_path, key_column):
-    df = pd.read_csv(csv_file_path, keep_default_na=False, na_values=[''])  # Treat only empty strings as NaN
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("version_string")
+args = parser.parse_args()
+
+def csv2yaml(csv_file_path, yaml_file_path, key_column, comment1):
+    df = pd.read_csv(csv_file_path, keep_default_na=False, na_values=['']) # Treat only empty strings as NaN
     df = df.where(pd.notnull(df), None) # Convert NaNs to None (null)
-    data = {row[key_column]: row.to_dict() for index, row in df.iterrows()}
-    with open(yaml_file_path, 'w') as file:
+    data = {row[key_column]: row.to_dict() for index, row in df.iterrows()} # Use key_column as key for each row
+    with open(yaml_file_path, 'w', encoding='utf-8') as file:
+        file.write('# ' + comment1 + "\n")
+        file.write('# YAML | ' + args.version_string + "\n")
+        file.write('# https://github.com/ccmars/world-data' + "\n")
         yaml.dump(data, file)
 
-csv2yaml('src/data/world_countries.csv', 'dist/yml/world_countries.yml', 'alpha2')
-csv2yaml('src/data/world_currencies.csv', 'dist/yml/world_currencies.yml', 'alpha3')
-csv2yaml('src/data/world_languages.csv', 'dist/yml/world_languages.yml', 'alpha2')
+csv2yaml('src/data/world_countries.csv', 'dist/yml/world_countries.yml', 'alpha2', 'World Countries (ISO 3166)')
+csv2yaml('src/data/world_currencies.csv', 'dist/yml/world_currencies.yml', 'alpha3', 'World Currencies (ISO 4217)')
+csv2yaml('src/data/world_languages.csv', 'dist/yml/world_languages.yml', 'alpha2', 'World Languages (ISO 639)')
