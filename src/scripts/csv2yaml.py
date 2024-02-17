32e8d74b4fbf7cdbb25aeda7055986d292265266
Cole Marshall <cm@colemarshall.net>
2024-02-16 22:18:32 -0600

    HTML conversion

--- src/scripts/csv2yaml.py
+++ src/scripts/csv2yaml.py
@@ -7,12 +7,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("version_string")
 args = parser.parse_args()
 
-def csv2yaml(csv_file_path, yaml_file_path, key_column, comment1):
+def csv2yaml(csv_file_path, yaml_file_path, key_column, title):
     df = pd.read_csv(csv_file_path, keep_default_na=False, na_values=['']) # Treat only empty strings as NaN
     df = df.where(pd.notnull(df), None) # Convert NaNs to None (null)
     data = {row[key_column]: row.to_dict() for index, row in df.iterrows()} # Use key_column as key for each row
     with open(yaml_file_path, 'w', encoding='utf-8') as file:
-        file.write('# ' + comment1 + "\n")
+        file.write('# ' + title + "\n")
         file.write('# YAML | ' + args.version_string + "\n")
         file.write('# https://github.com/ccmars/world-data' + "\n")
         yaml.dump(data, file)
