9dbb48eeec77b73e5f0908003c00c703a17e61ce
Cole Marshall <cm@colemarshall.net>
2024-02-17 23:26:01 -0600

    XLSX - Adjust initial width for columns based on values

--- src/scripts/csv2xlsx.py
+++ src/scripts/csv2xlsx.py
@@ -8,6 +8,17 @@ def csv2xlsx(csv_file_path, xlsx_file_path, sheet_name):
 
     book = load_workbook(xlsx_file_path)
     book.properties.creator = "github.com/ccmars/world-data"
+
+    worksheet = book[sheet_name]
+    for column in df:
+        max_length = 0
+        column = df[column]
+        if column.dtype != 'object':
+            continue
+        max_length = max(column.astype(str).map(len).max(), len(column.name))
+        col_idx = df.columns.get_loc(column.name)
+        worksheet.column_dimensions[chr(col_idx + 65)].width = max_length
+
     book.save(xlsx_file_path)
 
 csv2xlsx(
